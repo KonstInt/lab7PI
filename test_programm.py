6a02c05c892669c5378a73423d78fead0c5ea96b
Konstantin <kostya@MacBook-Pro-Konstantin.local>
2023-12-27 23:41:05 +0300

    add test

---
+++ test_programm.py
@@ -0,0 +1,11 @@
+from simple_programm import is_prime
+
+
+def test_one():
+   assert is_prime(5) == True
+
+def test_two():
+   assert is_prime(6) == False
+
+def test_three():
+   assert is_prime(1) == True
